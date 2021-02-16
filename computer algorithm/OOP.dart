178c2511de3824c8d9759f538689dca1e2607645
fausio luis <luisfausio@gmail.com>
2021-02-16 20:01:17 +0200

    Criação de classe em Dart

---
+++ computer algorithm/OOP.dart
@@ -0,0 +1,25 @@
+
+void main(List<String> args) {
+  
+}
+
+
+
+
+// Criação de classe em Dart
+class Pessoa {
+
+  // propriedades
+  String Nome;
+  int Idade;
+  double altura;
+
+  // metodos
+  void dormir(){
+    print("$Nome esta a dormir ZzZzZzZz");
+  }
+
+  void niver() {
+    Idade ++;
+    } 
+}
