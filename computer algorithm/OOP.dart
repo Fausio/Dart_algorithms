c686ad9b3962d51f3838df610157789c4e11780e
fausio luis <luisfausio@gmail.com>
2021-02-16 20:07:01 +0200

    uso de classe

--- computer algorithm/OOP.dart
+++ computer algorithm/OOP.dart
@@ -1,25 +1,44 @@
-
 void main(List<String> args) {
-  
+  // modo de instanciar em Dart, o new é opcional
+  Pessoa pessoa_um = new Pessoa();
+  pessoa_um.Nome = "Fáusio";
+  pessoa_um.Idade = 58;
+  pessoa_um.altura = 35.2;
+
+  Pessoa pessoa_dois = new Pessoa();
+  pessoa_dois.Nome = "Léo";
+  pessoa_dois.Idade = 14;
+  pessoa_dois.altura = 5.4;
+
+  print(pessoa_um.Nome);
+  print(pessoa_dois.Nome);
+
+  print(pessoa_um.Idade);
+  print(pessoa_dois.Idade);
+
+  pessoa_um.dormir();
+  pessoa_dois.dormir();
+
+  pessoa_um.niver();
+  pessoa_dois.niver();
+
+  print(pessoa_um.Idade);
+  print(pessoa_dois.Idade);
 }
-
-
-
 
 // Criação de classe em Dart
 class Pessoa {
-
   // propriedades
   String Nome;
   int Idade;
   double altura;
 
   // metodos
-  void dormir(){
+  void dormir() {
     print("$Nome esta a dormir ZzZzZzZz");
   }
 
   void niver() {
-    Idade ++;
-    } 
+    Idade++;
+  }
 }
