5c65b98b6a575e29784049781d41910311b8fbeb
fausio luis <luisfausio@gmail.com>
2021-02-17 20:46:09 +0200

    // para herdar os metodos do construtor basta criar o mesmo na nossa classe e mostrar pela palavra reservada que vem do supor (a classe que estamas a erdar)

--- computer algorithm/OOP_heritage.dart
+++ computer algorithm/OOP_heritage.dart
@@ -1,6 +1,5 @@
 void main() {
-  cao boby = new cao();
-      boby.Nome = "Max";
+  cao boby = new cao("Max",45); 
   boby.Vacinado();
 }
 
@@ -8,9 +7,8 @@ class animal {
   String _nome;
   double peso;
   int idade;
-  
+
   set Nome(String n) => _nome = n;
-  
 
   void FezSom() {
     print("$_nome fez algum som");
@@ -19,18 +17,28 @@ class animal {
   void Comer() {
     print("$_nome esta a comer");
   }
+
+  animal(this._nome, this.idade);
 }
 
 // para mostrar de qual classe estamos a erdar basta  usarmos a palavra reservada extends e de deseguida o nome da classe
 class cao extends animal {
+  
+// para herdar os metodos do construtor basta criar o mesmo na nossa classe e mostrar pela palavra reservada que vem do supor (a classe que estamas a erdar)
+  cao(String nome, int idade) : super(nome, idade);
+
   void Vacinado() {
-    print("O $_nome esta vacinado");
+    print("O $_nome esta vacinado ");
   }
 }
 
-
 // para mostrar de qual classe estamos a erdar basta  usarmos a palavra reservada extends e de deseguida o nome da classe
-class Gato extends animal {
+class gato extends animal {
+  
+   // para herdar os metodos do construtor basta criar o mesmo na nossa classe e mostrar pela palavra reservada que vem do supor (a classe que estamas a erdar)
+
+  gato(String nome, int idade) : super(nome, idade);
+  
   void amigavel() {
     print("o $_nome está amigavel");
   }
