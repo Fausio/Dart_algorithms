b6efbe2ca4b36e4c1311212d84ddfde02d85eeda
fausio luis <luisfausio@gmail.com>
2021-02-17 21:38:17 +0200

    Static, Final e Const

---
+++ computer algorithm/Modifiers.dart
@@ -0,0 +1,20 @@
+main(List<String> args) {
+  
+// em tempo de execução, a palavra reservada final faz nos nao alterar  o onjecto
+final human alguem = new human();
+}
+
+
+
+class values{
+  
+  //static nao precisamos instanciar o objecto da classe para usar essa propriedade
+  static int etc;
+
+// a palavra reservda const faz com que a propriedade nao seja mudada de valor, é uma constante em tempo de execução ou compilação
+  const etc_dois = 5;
+}
+
+class human{
+
+}
